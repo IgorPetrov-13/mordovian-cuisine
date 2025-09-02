e84c26e673fb3d952e7d9d1beee76a40ebfc1080
Петров Игорь <igor.petrov@kartoteka.adds>
2025-09-02 19:55:14 +0300

    #first commit

---
+++ src/config/site.config.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: 'Мордовская кухня',
+  description: 'Рецепты мордовской кухни',
+  navItems: [
+    { href: '/', label: 'Рецепты' },
+    { href: '/ingredients', label: 'Ингредиенты' },
+    { href: '/about', label: 'О нас' },
+  ],
+};
